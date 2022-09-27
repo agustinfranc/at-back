3b748f4deaebd91615e5532be7f2f3edc259c485
Agustin <agustin.amato96@gmail.com>
2022-09-27 17:08:33 -0300

    Update app/Models/Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['name','dni','phone','rate','taxable','comments'];
 }
