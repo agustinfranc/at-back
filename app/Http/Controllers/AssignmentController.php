8242dc07d6dc95aae45c9053a2d9bfcda2569201
Agustin <agustin.amato96@gmail.com>
2022-10-11 10:23:51 -0300

    Update app/Http/Controllers/AssignmentController.php

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -14,7 +14,7 @@ class AssignmentController extends Controller
 {
     public function __construct(
         private readonly GetAssignmentRepository $getRepository,
-        private readonly StoreAssignmentRepository $storeRepository,
+        private readonly StoreAssignmentRepository $storeRepository
     ) {
     }
 
