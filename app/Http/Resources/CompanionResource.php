b1d53e0326f75844af7ee71db4b1918d0b55e719
Agustin <agustin.amato96@gmail.com>
2022-10-17 16:43:42 -0300

    Deploy (#34)
    * feat: add spanish localization values
    
    * adding new columns & droping dni (#13)
    
    * adding new columns & droping dni
    
    * feat: fixes for the pr
    
    * store request fix
    
    * adding the softDeletes to models
    
    * fixing soft deletes
    
    * Update app/Models/Client.php
    
    * Update app/Models/Companion.php
    
    * Update app/Models/User.php
    
    * add new columns to client table (#18)
    
    * adding new columns to client table
    
    * fixes for the pr
    
    * changing birth for birthday
    
    * changing job_desc for treatment
    
    * adding has_sign_contract to companion
    
    * correcciones
    
    Co-authored-by: Agustin Amato <agustin.amato96@gmail.com>
    
    * fix out of range cuit
    
    * fix: client form
    
    * Feature/assignments (#21)
    
    * Starting the assignment entity
    
    * Update app/Repositories/Assignment/StoreAssignmentRepository.php
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * advancing the assignment entity
    
    * feat: more advances
    
    * feat: making the weekdays table
    
    * Adding the AssignmentWeekdays table
    
    * fixes for the pr
    
    * advancing on the weekdays table
    
    * Making assignment get working
    
    * feat: add new day, day_type models and remove assignment_weekday
    
    * feat: add relationships, assignment_day table
    
    * feat: add seeders and factories for assignment
    
    * doc: add item migrations
    
    * feat: assignment relationsships and get repository
    
    * feat: working with assigment store
    
    * feat: remove day_type
    
    * fix to assignment store
    
    * feat: store assignment validation
    
    * feat: store assignment days
    
    * fix: client form
    
    Co-authored-by: francocava <francocavallini@hotmail.com>
    Co-authored-by: Franco Cavallini <43447255+francocava@users.noreply.github.com>
    
    * fix: add transaction manual handler
    
    * feat: add show logic to assignments
    
    * doc: add link
    
    * feat(assignment): add resource to index method
    
    * show client/companion (#24)
    
    * clients show now working
    
    * adding show for companions
    
    * adding show to assignments
    
    * removing show from assigments & showRepo
    
    * removing assigment showRepo & changing show for getOne
    
    * Update app/Http/Controllers/AssignmentController.php
    
    * Update app/Http/Controllers/AssignmentController.php
    
    * Update app/Http/Controllers/AssignmentController.php
    
    * Update app/Http/Controllers/CompanionController.php
    
    * Update app/Http/Controllers/ClientController.php
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * feat(assignments): add casts
    
    * feat(clients): add resource
    
    * Add resource to companion (#25)
    
    * feat: add localization for attributes spanish
    
    * feat: add assignment update logic (#27)
    
    * Add decimals to rules (#28)
    
    * adding decimals to rules
    
    * going back to digits_between on companion
    
    * nullable is now first in rules
    
    * Feature/decimals (#29)
    
    * adding decimals to rules
    
    * going back to digits_between on companion
    
    * nullable is now first in rules
    
    * removing required
    
    * Update app/Http/Requests/Client/StoreClientRequest.php
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * adding change() to down
    
    * rename migration
    
    Co-authored-by: Agustin <agustin.amato96@gmail.com>
    
    * making taxable nullable (#30)
    
    * making taxable nullable
    
    * adding S to client
    
    * feat(assignment): add delete action
    
    * Feature/companion-edit-delete (#33)
    
    * feat: add delete action and e2e tests
    
    * fix: delete commented code
    
    * fix: companion request
    
    Co-authored-by: Franco Cavallini <43447255+francocava@users.noreply.github.com>
    Co-authored-by: francocava <francocavallini@hotmail.com>

--- app/Http/Resources/CompanionResource.php
+++ app/Http/Resources/CompanionResource.php
@@ -6,26 +6,28 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CompanionResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-          'id' => $this->id,
-          'name' => $this->name,
-          'cuit' => $this->cuit,
-          'nationality' => $this->nationality,
-          'birthday' => $this->birthday,
-          'phone' => $this->phone,
-          'max_taxable' => $this->max_taxable,
-          'monotax' => $this->monotax,
-          'criminal_record' => $this->criminal_record,
-          'insurance' => $this->insurance,
-          'has_sign_contract' => $this->has_sign_contract,
-        ];
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request)
+  {
+    return [
+      'id' => $this->id,
+      'name' => $this->name,
+      'cuit' => $this->cuit,
+      'nationality' => $this->nationality,
+      'birthday' => $this->birthday,
+      'phone' => $this->phone,
+      'max_taxable' => $this->max_taxable,
+      'monotax' => $this->monotax,
+      'criminal_record' => $this->criminal_record,
+      'insurance' => $this->insurance,
+      'has_sign_contract' => $this->has_sign_contract,
+      'created_at' => $this->created_at,
+      'updated_at' => $this->updated_at,
+    ];
+  }
 }
