b3004bac3ec8c3fdd7fabafd198eb668e240b099
francocava <francocavallini@hotmail.com>
2022-11-09 10:55:02 -0300

    another extra "s"

--- database/migrations/2022_11_04_005215_create_assignment_templates_table.php
+++ database/migrations/2022_11_04_005215_create_assignment_templates_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('assignments_templates');
+        Schema::dropIfExists('assignment_templates');
     }
 };
