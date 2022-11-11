02012f002b9245d0b49719277e024c678c67d79b
francocava <francocavallini@hotmail.com>
2022-11-11 10:32:44 -0300

    fixing day factory

--- database/factories/AssignmentTemplateDayFactory.php
+++ database/factories/AssignmentTemplateDayFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AssignmentDay>
  */
-class AssignmentDayFactory extends Factory
+class AssignmentTemplateDayFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,7 +18,7 @@ class AssignmentDayFactory extends Factory
     public function definition()
     {
         return [
-            'assignment_id' => AssignmentTemplate::factory(),
+            'assignment_template_id' => AssignmentTemplate::factory(),
             'day_id' => fake()->numberBetween('1', '7'),
             'hours' => fake()->numberBetween('1', '24'),
             'from' => fake()->time(),
