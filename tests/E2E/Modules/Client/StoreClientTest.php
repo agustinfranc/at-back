c4d155e5f7dcda8a1670414f51366efb215c3a57
Agustin Amato <agustin.amato96@gmail.com>
2022-11-16 22:12:20 -0300

    test(client) store tests

--- tests/E2E/Modules/Client/StoreClientTest.php
+++ tests/E2E/Modules/Client/StoreClientTest.php
@@ -13,13 +13,31 @@ class StoreClientTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_store_client()
+    public function test_can_store_client()
     {
-        //
+        $client = Client::factory()->make()->toArray();
+
+
+        $response = $this->postJson('/api/clients', $client);
+
+
+        $response->assertCreated()
+            ->assertJson(['data' => $client], false);
     }
 
-    public function test_update_client()
+    public function test_can_update_client()
     {
-        //
+        $client = Client::factory()->create();
+        $client->name = 'George';
+        $client->save();
+        $clientsArrayFromResource = json_decode((new ClientResource($client))->toJson(), true);
+
+
+        $response = $this->putJson('/api/clients/' . $client->id, $client->toArray());
+
+
+        $response->assertOk()
+            ->assertJson(['data' => $clientsArrayFromResource], false);
+        // testear que el name sea George
     }
 }
