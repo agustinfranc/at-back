a9d4f41262c196bf7ac04721f633386527d5b961
Agustin Amato <agustin.amato96@gmail.com>
2022-12-16 20:53:37 -0300

    test: add test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
+use App\Modules\Client\Interfaces\IGetClientRepository;
+use App\Repositories\Client\GetClientRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        IGetClientRepository::class => GetClientRepository::class
+    ];
+
     /**
      * Register any application services.
      *
@@ -13,7 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // $this->app->bind(
+        //     'App\Modules\Client\Interfaces\IGetClientRepository',
+        //     'App\Repositories\Client\GetClientRepository'
+        // );
     }
 
     /**
---
+++ tests/Unit/Client/GetClientTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Client;
+
+use App\Http\Controllers\ClientController;
+use App\Models\Client;
+use App\Modules\Client\Interfaces\IGetClientRepository;
+use App\Repositories\Client\GetClientRepository;
+use App\Repositories\Client\StoreClientRepository;
+
+test('find a client', function () {
+    $this->setUp();
+
+    $fakeClient = Client::factory()->make();
+    $controller = new ClientController(
+        // new HardcodedInMemoryGetClientRepository($fakeClient),
+        // app()->make(HardcodedInMemoryGetClientRepository::class),
+        new GetClientRepository(),
+        // app()->make(StoreClientRepository::class),
+        new StoreClientRepository()
+    );
+    // $controller = app()->make(ClientController::class);
+    // $expectedClient = $controller->show($fakeClient);
+
+    // $user = User::factory()->make([
+    //     'email_verified_at' => null,
+    // ])->toArray();
+    // $request = $user;
+    // $request['password'] = 'password';
+    // $request['repeate_password'] = 'password';
+
+
+    // $response = $this->post('/api/users/', $request);
+
+
+    // $response
+    //     ->assertStatus(201)
+    //     ->assertJson(['data' => $user], false);
+});
