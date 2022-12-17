9b18af84ecc6ba25f83cf68aa481c084952a9530
Agustin Amato <agustin.amato96@gmail.com>
2022-12-16 21:43:01 -0300

    refactor: StoreTemplateMigrationRepository

--- app/Repositories/TemplateMigration/StoreTemplateMigrationRepository.php
+++ app/Repositories/TemplateMigration/StoreTemplateMigrationRepository.php
@@ -11,34 +11,45 @@ use Illuminate\Database\Eloquent\Collection;
 
 class StoreTemplateMigrationRepository
 {
-  public function store(AssignmentTemplate $template): bool
-  {
-    return $this->checkForMigrations($template);
-  }
+    public function store(AssignmentTemplate $template): bool
+    {
+        // TODO: retornar no un booleano sino las migraciones que se crearon
+        return $this->makeMigrations($template);
+    }
 
-  private function checkForMigrations(AssignmentTemplate $template): bool
-  {
-    $migrations = TemplateMigration::where('assignment_template_id', $template->id)->get();
-    logger($migrations);
-    foreach ($migrations as $migration) {
-      $migrationDate = Carbon::parse($migration->migrationDate);
-      $date = Carbon::parse(date("Y-m-d"));
-      if ($migrationDate->month == $date->month) {
-        return false;
-      } else {
-        TemplateMigration::create([
-          'assignment_template_id' => $template->id,
-          'migration_date' => date("Y-m-d"),
-        ]);
+    // TODO:
+    //* en realidad no necesitaste las migraciones de los meses anteriores, solo la del mes corriente
+    //* no te importa si el mes anterior se creo o no, la vas a crear de todas maneras
+    private function makeMigrations(AssignmentTemplate $template): bool
+    {
+        $migrations = TemplateMigration::where('assignment_template_id', $template->id)->get();
+
+        // Si el template no tiene migraciones creo una y retorno
+        if ($migrations->isEmpty()) {
+            TemplateMigration::create([
+                'assignment_template_id' => $template->id,
+                'migration_date' => date("Y-m-d"),
+            ]);
+
+            return true;
+        }
+
+        // Si tiene las recorro, y si es la migracion del mes corriente entonces no creo una migracion
+        // de lo contrario creo una migracion con fecha del dia actual
+        foreach ($migrations as $migration) {
+            $migrationMonth = Carbon::parse($migration->migrationDate)->month;
+            $currentMonth = Carbon::parse(date("Y-m-d"))->month;
+
+            if ($migrationMonth === $currentMonth) return false;
+
+            TemplateMigration::create([
+                'assignment_template_id' => $template->id,
+                'migration_date' => date("Y-m-d"),
+            ]);
+
+            return true;
+        }
+
         return true;
-      }
     }
-    if ($migrations->isEmpty()) {
-      TemplateMigration::create([
-        'assignment_template_id' => $template->id,
-        'migration_date' => date("Y-m-d"),
-      ]);
-    }
-    return true;
-  }
 }
