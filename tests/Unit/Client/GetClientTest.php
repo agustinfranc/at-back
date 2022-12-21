dc3707d6ed468ba3e8a9452036cd032c54decf67
Agustin Amato <agustin.amato96@gmail.com>
2022-12-21 18:11:40 -0300

    fix test

--- tests/Unit/Client/GetClientTest.php
+++ tests/Unit/Client/GetClientTest.php
@@ -1,39 +1,44 @@
 <?php
 
-namespace Tests\Unit\Client;
+namespace Tests\Unit;
 
-use App\Http\Controllers\ClientController;
-use App\Models\Client;
-use App\Modules\Client\Interfaces\IGetClientRepository;
-use App\Repositories\Client\GetClientRepository;
-use App\Repositories\Client\StoreClientRepository;
+use PHPUnit\Framework\TestCase;
 
-// test('find a client', function () {
-    // $this->setUp();
+class GetClientTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_find_client()
+    {
+        $this->assertTrue(true);
 
-    // $fakeClient = Client::factory()->make();
-    // $controller = new ClientController(
-    // new HardcodedInMemoryGetClientRepository($fakeClient),
-    // app()->make(HardcodedInMemoryGetClientRepository::class),
-    // new GetClientRepository(),
-    // app()->make(StoreClientRepository::class),
-    // new StoreClientRepository()
-    // );
-    // $controller = app()->make(ClientController::class);
-    // $expectedClient = $controller->show($fakeClient);
+        // $fakeClient = Client::factory()->make();
+        // $controller = new ClientController(
+        // new HardcodedInMemoryGetClientRepository($fakeClient),
+        // app()->make(HardcodedInMemoryGetClientRepository::class),
+        // new GetClientRepository(),
+        // app()->make(StoreClientRepository::class),
+        // new StoreClientRepository()
+        // );
+        // $controller = app()->make(ClientController::class);
+        // $expectedClient = $controller->show($fakeClient);
 
-    // $user = User::factory()->make([
-    //     'email_verified_at' => null,
-    // ])->toArray();
-    // $request = $user;
-    // $request['password'] = 'password';
-    // $request['repeate_password'] = 'password';
-
-
-    // $response = $this->post('/api/users/', $request);
+        // $user = User::factory()->make([
+        //     'email_verified_at' => null,
+        // ])->toArray();
+        // $request = $user;
+        // $request['password'] = 'password';
+        // $request['repeate_password'] = 'password';
 
 
-    // $response
-    //     ->assertStatus(201)
-    //     ->assertJson(['data' => $user], false);
-// });
+        // $response = $this->post('/api/users/', $request);
+
+
+        // $response
+        //     ->assertStatus(201)
+        //     ->assertJson(['data' => $user], false);
+    }
+}
