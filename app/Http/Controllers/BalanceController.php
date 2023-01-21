9b32813c30bf6ddceb601c40d78441f56ad9b40c
francocava <francocavallini@hotmail.com>
2023-01-20 22:53:20 -0300

    Separating clients & companions balances repos

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Assignment\GetAssignmentRepository;
-use App\Repositories\Balance\GetBalanceRepository;
+use App\Repositories\Balance\GetClientsBalanceRepository;
+use App\Repositories\Balance\GetCompanionsBalanceRepository;
 use App\Repositories\Client\GetClientRepository;
 use App\Repositories\Companion\GetCompanionRepository;
 use Illuminate\Support\Collection;
@@ -14,17 +15,18 @@ final class BalanceController extends Controller
         private readonly GetAssignmentRepository $getAssignmentRepository,
         private readonly GetClientRepository $getClientRepository,
         private readonly GetCompanionRepository $getCompanionRepository,
-        private readonly GetBalanceRepository $getBalanceRepository,
+        private readonly GetClientsBalanceRepository $getClientsBalanceRepository,
+        private readonly GetCompanionsBalanceRepository $getCompanionsBalanceRepository,
     ) {
     }
 
     public function getClientsBalance(): Collection
     {
-        return $this->getBalanceRepository->getClientBalances($this->getClientRepository::getAll());
+        return $this->getClientsBalanceRepository->getClientBalances($this->getClientRepository::getAll());
     }
 
     public function getCompanionsBalance(): Collection
     {
-        return $this->getBalanceRepository->getCompanionBalances($this->getCompanionRepository::getAll());
+        return $this->getCompanionsBalanceRepository->getCompanionBalances($this->getCompanionRepository::getAll());
     }
 }
--- app/Repositories/Balance/GetCompanionsBalanceRepository.php
+++ app/Repositories/Balance/GetCompanionsBalanceRepository.php
@@ -4,61 +4,15 @@ declare(strict_types=1);
 
 namespace App\Repositories\Balance;
 
-use App\Models\Assignment;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use App\Repositories\Assignment\GetAssignmentRepository;
 
-final class GetBalanceRepository
+final class GetCompanionsBalanceRepository
 {
     public function __construct(
         private readonly GetAssignmentRepository $getRepository,
     ) {
-    }
-
-    public function getClientBalances(Collection $clients)
-    {
-        $currentDate = Carbon::parse(date("Y-m-d"));
-
-        return $clients
-            ->filter(fn ($client) => self::calculateHours(self::getClientAssignments($client, $currentDate)) > 0)
-            ->map(
-                fn ($client) =>
-                [
-                    'name' => $client->name,
-                    'taxable_debt' => (self::getClientTaxableDebt($client, $currentDate)),
-                    'no_tax_debt' => (self::getClientNoTaxDebt($client, $currentDate))
-                ]
-            )->values();
-    }
-
-    private function getClientTotalDebt($client, $currentDate)
-    {
-        return $client->rate * self::calculateHours(self::getClientAssignments($client, $currentDate));
-    }
-
-    private function getClientTaxableDebt($client, $currentDate)
-    {
-        return self::getClientTotalDebt($client, $currentDate) * ($client->taxable / 100);
-    }
-
-    private function getClientNoTaxDebt($client, $currentDate)
-    {
-        return self::getClientTotalDebt($client, $currentDate) - self::getClientTaxableDebt($client, $currentDate);
-    }
-
-    private function getClientAssignments($client, $currentDate)
-    {
-        return $this->getRepository::getClientAssignments($client, $currentDate);
-    }
-
-    private function calculateHours($assignments): int
-    {
-        return $assignments->reduce(
-            fn ($totalHours, $assignment) =>
-            $totalHours + $assignment->hours,
-            0
-        );
     }
 
     public function getCompanionBalances(Collection $companions)
