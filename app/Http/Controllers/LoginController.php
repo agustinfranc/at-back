f1c36f4737485517311fb96af8c8f6e20260b34c
francocava <francocavallini@hotmail.com>
2023-03-27 12:01:21 -0300

    changes for the test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,32 +10,32 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-  public function authenticate(Request $request, GetUserRepository $repository)
-  {
-    $validatedData = $request->validate([
-      'email' => 'required|email',
-      'password' => 'required',
-      'device_name' => 'nullable',
-    ]);
+    public function authenticate(Request $request, GetUserRepository $repository)
+    {
+        $validatedData = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'nullable',
+        ]);
 
-    $user = $repository->getByEmail($validatedData['email']);
+        $user = $repository->getByEmail($validatedData['email']);
 
-    if (!$user || !Hash::check($validatedData['password'], $user->password)) {
-      return response()->json(['error' => 'Usuario y/o contraseña incorrectos'], 401);
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            return response()->json(['error' => 'Usuario y/o contraseña incorrectos'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken($request->device_name ?? 'generic')->plainTextToken]);
     }
 
-    return response()->json(['token' => $user->createToken($request->device_name ?? 'generic')->plainTextToken]);
-  }
+    public function me(Request $request)
+    {
+        $user = $request->user();
 
-  public function me(Request $request)
-  {
-    $user = $request->user();
+        return response()->json(['user' => $user]);
+    }
 
-    return response()->json(['user' => $user]);
-  }
-
-  public function logout(Request $request)
-  {
-    $request->user()->currentAccessToken()->delete();
-  }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
 }
