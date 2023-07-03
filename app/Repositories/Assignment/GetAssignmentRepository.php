f92af4ece1f096375b091a4d53dd25b6f842d5b7
Agustin Amato <agustin.amato96@gmail.com>
2023-07-03 12:28:05 -0300

    refact: add types

--- app/Repositories/Assignment/GetAssignmentRepository.php
+++ app/Repositories/Assignment/GetAssignmentRepository.php
@@ -5,34 +5,35 @@ declare(strict_types=1);
 namespace App\Repositories\Assignment;
 
 use App\Models\Assignment;
+use Illuminate\Database\Eloquent\Collection;
 
 class GetAssignmentRepository
 {
-    public static function getAll()
+    public static function getAll(): Collection
     {
         return Assignment::with(['client', 'companion'])->get();
     }
 
-    public static function getOne($id)
+    public static function getOne($id): ?Assignment
     {
         return Assignment::with(['client', 'companion'])->find($id);
     }
 
-    public static function existPeriodicAssignment($clientId, $companionId)
+    public static function existPeriodicAssignment($clientId, $companionId): ?Assignment
     {
         return Assignment::where('client_id', $clientId)
             ->where('companion_id', $companionId)
             ->first();
     }
 
-    public static function getClientAssignments($client, $currentDate)
+    public static function getClientAssignments($client, $currentDate): Collection
     {
         return Assignment::where('client_id', '=', $client->id)
             ->whereMonth('date', $currentDate)
             ->whereYear('date', $currentDate->year)->get();
     }
 
-    public static function getCompanionAssignments($companion, $currentDate)
+    public static function getCompanionAssignments($companion, $currentDate): Collection
     {
         return Assignment::with(['client'])->where('companion_id', '=', $companion->id)
             ->whereMonth('date', $currentDate->month)
--- app/Repositories/Companion/GetCompanionRepository.php
+++ app/Repositories/Companion/GetCompanionRepository.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace App\Repositories\Companion;
 
 use App\Models\Companion;
+use Illuminate\Database\Eloquent\Collection;
 
 class GetCompanionRepository
 {
-    public static function getAll()
+    public static function getAll(): Collection
     {
         return Companion::all();
     }
 
-    public static function getOne($id)
+    public static function getOne($id): ?Companion
     {
         return Companion::find($id);
     }
