f0849b1b4b8895432132aad7cd4f7ba5b31f6a53
francocava <francocavallini@hotmail.com>
2023-08-03 18:27:20 -0300

    adding migration

---
+++ database/migrations/2023_08_03_182218_add_companion_rate_to_clients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->float('companion_rate', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropColumn('companion_rate');
+        });
+    }
+};
